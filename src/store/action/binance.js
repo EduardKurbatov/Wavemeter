ab0246b3f2bfa4828003c197a230328d7b25933a
Edik123123 <edikcrimedog@gmail.com>
2021-05-24 17:08:40 +0300

    calling klines API with params

--- src/store/action/binance.js
+++ src/store/action/binance.js
@@ -5,7 +5,7 @@ export const setExchangeInfo = () => async (dispatch) => {
   const response = await binanceAPI.getExchangeInfo();
 
   if (!response.isError) {
-    const exchangeInfoData = response.data.symbols.map(item => ({
+    const exchangeInfo = response.data.symbols.map(item => ({
       baseAsset: item.baseAsset,
       quoteAsset: item.quoteAsset,
       symbol: item.symbol
@@ -13,7 +13,7 @@ export const setExchangeInfo = () => async (dispatch) => {
 
     dispatch({
       type: ActionTypes.GET_EXCHANGE_INFO,
-      payload: {exchangeInfoData}
+      payload: {exchangeInfo}
     })
   };
 };
