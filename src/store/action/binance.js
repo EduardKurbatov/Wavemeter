d517adc832fd39f4f912395a347d7528018d13f7
Edik123123 <edikcrimedog@gmail.com>
2021-05-24 16:20:25 +0300

    requested changes

--- src/store/action/binance.js
+++ src/store/action/binance.js
@@ -3,15 +3,16 @@ import { binanceAPI } from '../api/binance';
 
 export const setExchangeInfo = () => async (dispatch) => {
   const response = await binanceAPI.getExchangeInfo();
+  if (!response.isError) {
+    const exchangeInfoData = response.data.symbols.map(item => ({
+      baseAsset: item.baseAsset,
+      quoteAsset: item.quoteAsset,
+      symbol: item.symbol
+    }))
 
-  dispatch({
-    type: ActionTypes.GET_EXCHANGE_INFO,
-    payload: {exchangeInfoData: response.data.symbols.map((item) => {
-      return {
-        baseAsset: item.baseAsset,
-        quoteAsset: item.quoteAsset,
-        symbol: item.symbol
-      }
-    })}
-  })
+    dispatch({
+      type: ActionTypes.GET_EXCHANGE_INFO,
+      payload: {exchangeInfoData}
+    })
+  };
 };
